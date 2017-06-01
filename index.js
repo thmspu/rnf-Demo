1db58d5e8d13630eaafb449562ee45f9030fabd8
thomas <thomase3141@gmail.com>
2017-06-01 00:10:20 -0400

    update

--- index.js
+++ index.js
@@ -1,53 +1,57 @@
-var quiz = [false,false,false,false];
-var image = ["reagan.jpg","Teddy.jpg","w-bush.jpg","lincoln.jpg","ike.jpg","ford.jpg","hw-bush.jpg"];
-var imageIndex = Math.round(Math.random()*6);
-$('#resultImage').attr("src","imgs/" + image[imageIndex]) ;
-$('.result').hide();
-$('.Q1').click(function() {
-	if($(this).find('input:checkbox:checked')) {
-		quiz[0] = true;
+ 	let quiz = [false,false,false,false];
+	const image = ["reagan.jpg","Teddy.jpg","w-bush.jpg","lincoln.jpg","ike.jpg","ford.jpg","hw-bush.jpg"];
+	let imageIndex = Math.round(Math.random()*6);
 
-		for(var a of quiz) {
-            if(a === false ) {
-            	return;
-            }
-		}
-	 imageIndex = Math.round(Math.random()*6);
-      $('.result').show();
-	}
-});
-$('.Q4').click(function() {
-	if($(this).find('input:checkbox:checked')) {
-		quiz[3] = true;
-		for(var a of quiz) {
-            if(a === false ) {
-            	return;
-            }
-		}
+    const show = () => {
 		imageIndex = Math.round(Math.random()*6);
-      $('.result').show();
+		$('.result').show();
 	}
-});
 
-$('.Q2').click(function() {	
+	const judge = () => {
+		 for(let a of quiz) {
+			if(a === false ) {
+				return;
+			}
+		}
+		show();
+	}
+
+	
+
+	$('#resultImage').attr("src","imgs/" + image[imageIndex]);
+	
+	$(function(){
+			$("li").click(function(){
+				console.log($(this).className)
+				$(this).parent().find("li").each(function(){
+					$(this).prop("class", "");
+					$(this).find("input").prop("checked", "");
+				});
+				$(this).prop("class", "active");
+				$(this).find("input").prop("checked", "1");
+			});
+		});
+
+	$('.result').hide();
+	$('.Q1').click(function() {
+		if($(this).find('input:checkbox:checked')) {
+			quiz[0] = true;
+			judge();
+		}
+	});
+	$('.Q4').click(function() {
+		if($(this).find('input:checkbox:checked')) {
+			quiz[3] = true;
+			judge();
+		}
+	});
+
+	$('.Q2').click(function() {	
 		quiz[1] = true;
-		for(var a of quiz) {
-            if(a === false ) {
-            	return;
-            }
-		}
-		imageIndex = Math.round(Math.random()*6);
-      $('.result').show();
-});
+		judge();
+	});
 
-$('.Q3').click(function() {
+	$('.Q3').click(function() {
 		quiz[2] = true;
-		
-		for(var a of quiz) {
-            if(a === false ) {
-            	return;
-            }
-		}
-		imageIndex = Math.round(Math.random()*6);
-      $('.result').show();
-});
+		judge();
+	});
